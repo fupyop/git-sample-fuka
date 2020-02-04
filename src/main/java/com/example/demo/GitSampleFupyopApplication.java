99b643ad1a679ec0d68bb6d796a4df6e78e572b7
fupyop <fuka.hirayama@rakus-partners.co.jp>
2020-02-04 11:53:10 +0900

    プロジェクトを新規に作成しました。 or first commit

---
+++ src/main/java/com/example/demo/GitSampleFupyopApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleFupyopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleFupyopApplication.class, args);
+	}
+
+}
